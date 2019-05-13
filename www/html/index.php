e4913e3670796cf321e0b55d7b78bf5932183f11
shuta13 <hollystarsun@gmail.com>
2019-05-13 19:47:09 +0900

    簡易ログインページ実装

--- www/html/index.php
+++ www/html/index.php
@@ -1,4 +1,16 @@
-<php
-phpinfo();
+<?php
+session_start();
 
-<h1> helloworld </h1>
+$error_message = "";
+
+if (isset($_POST["login"])) {
+  if ($_POST["user_name"] == "hoge" && $_POST["password"] == "password") {
+    $_SESSION["user_name"] = $_POST["user_name"];
+    $login_success_url = "login_success.php";
+    header("Location: {$login_success_url}");
+    exit;
+  }
+
+  $error_message = "Sorry, error.";
+}
+?>
