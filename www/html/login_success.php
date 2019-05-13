6b11d6b46c5d3e7be5a44d2b7718990ced561bb5
shuta13 <hollystarsun@gmail.com>
2019-05-14 00:55:22 +0900

    XSS実装

--- www/html/login_success.php
+++ www/html/login_success.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
-
-if(!isset($_SESSION["user_name"])) {
-	$no_login_url = "index.php";
+if (!$_SESSION) {
+  $no_login_url = "index.php";
 	header("Location: {$no_login_url}");
 	exit;
 }
@@ -14,8 +13,12 @@ if(!isset($_SESSION["user_name"])) {
   </head>
   <body>
     <p>Successed.</p>
+    <form action="login_success.php" method="POST">
+      お名前を入力してください<input type="text" name="name">
+      <input type="submit" value="送信">
+    </form>
+    <?php 
+      echo "名前：".$_POST['name'];
+    ?>
   </body>
 </html>
-<?php
-  echo $_GET["keyword"];
-?>
